8c6b5512aa122712c9c90b74864a70c0bec0deed
aniket <aniket.chanana@gmail.com>
2024-05-18 20:08:38 +0530

    Cached everything

--- sw.js
+++ sw.js
@@ -1,20 +1,26 @@
-const version = 'v1.0';
+const version = 'v1.1';
 const CACHE_NAME = `STATIC_CACHE_${version}`;
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         '/',
-        '/index.html',
-        '/css/style.css',
-        '/css/sectionStyles.css',
         '/css/1024_styles.css',
         '/css/768_styles.css',
-        '/script/app.js',
-        '/images/me_light.JPG',
-        '/images/me_dark.JPG',
+        '/css/sectionStyles.css',
+        '/css/style.css',
         '/images/favicon.png',
+        '/images/me_dark.JPG',
+        '/images/me_light.JPG',
+        '/script/app.js',
+        'https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300;400;700&display=swap',
+        'https://fonts.gstatic.com/s/comicneue/v8/4UaErEJDsxBrF37olUeD_wHL8pxULilENlY.woff2',
+        'https://fonts.gstatic.com/s/comicneue/v8/4UaErEJDsxBrF37olUeD_xHM8pxULilENlY.woff2',
+        'https://fonts.gstatic.com/s/comicneue/v8/4UaHrEJDsxBrF37olUeD96rp57F2IwM.woff2',
         'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
+        'https://use.fontawesome.com/releases/v5.8.1/webfonts/fa-brands-400.woff2',
+        'https://use.fontawesome.com/releases/v5.8.1/webfonts/fa-regular-400.woff2',
+        'https://use.fontawesome.com/releases/v5.8.1/webfonts/fa-solid-900.woff2',
       ]);
     })
   );
